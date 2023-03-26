a0897062cb6b4907fe9087cffd6b6a4c52fd8818
Crazy Urus <crazyurus@vip.qq.com>
2023-03-26 09:44:38 +0800

    docs: 增加友链

--- constants/friends.js
+++ constants/friends.js
@@ -10,5 +10,13 @@ module.exports = [
   {
     name: '匠心博客',
     url: 'https://zhaomenghuan.js.org'
+  },
+  {
+    name: '咀嚼之味',
+    url: 'https://jerryzou.com'
+  },
+  {
+    name: '喵了个咪乎',
+    url: 'https://y1ng.org'
   }
 ];
