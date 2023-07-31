1350678d7e303ecca8109f1ccc80383c0c8ab934
Crazy Urus <crazyurus@vip.qq.com>
2023-07-31 18:44:17 +0800

    fix: 解决图片不显示

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ function Document(): JSX.Element {
         <meta charSet="utf-8" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#000" />
+        <meta name="referrer" content="no-referrer"></meta>
         <link rel="icon" type="image/png" href="/avatar.png" />
       </Head>
       <body>
